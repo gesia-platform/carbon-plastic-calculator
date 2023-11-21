ee18fe836de5040374682a680bca596f3f983cc9
winterm1 <oiool@naver.com>
2023-11-21 14:28:45 +0900

    calculation modified

--- CarbonPlasticCalculator.sol
+++ CarbonPlasticCalculator.sol
@@ -8,6 +8,6 @@ contract CarbonPlasticCalculator {
     function calculateCO2ByPlastic(uint256 weight) public view returns (uint256) {
         // 플라스틱(PET) 무게(kg) x PET 병 1kg 탄소배출계수(kgCO2/kg) = 배출량(kgCO2)
         // weight scaled up by 10000
-        return ((weight * plasticCo2Emission) / 1e8) * 1e18;
+        return ((weight * plasticCo2Emission) / 1e18) * 1e8;
     }
 }
